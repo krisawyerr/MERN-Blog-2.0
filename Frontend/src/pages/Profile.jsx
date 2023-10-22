5ba5925f6654deead28d94c30f782dffb3894647
Kris Sawyerr <krisawyerr@gmail.com>
2023-10-22 09:08:42 -0700

    Added Sidenav to all pages

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import axios from "axios"
 import { IF, URL } from "../url"
 import { UserContext } from "../context/UserContext"
 import { useNavigate, useParams } from "react-router-dom"
+import SideNavbar from "../components/sidenav"
 
 function Profile() {
   const param=useParams().id
@@ -84,30 +85,33 @@ function Profile() {
   }, [param, user]);
 
   return (
-      <div>
-      <Navbar/>
-      <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
-        <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
-          <h1 className="text-xl font-bold mb-4">Your posts:</h1>
-          {posts?.map((p)=>(
-            <ProfilePosts key={p._id} p={p}/>
-          ))}
-        </div>
-        <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end ">
-          <div className=" flex flex-col space-y-4 items-start">
-              <h1 className="text-xl font-bold mb-4">Profile</h1>
-              <input onChange={(e)=>setUsername(e.target.value)} value={username} className="outline-none px-4 py-2 text-gray-500" placeholder="Your username" type="text"/>
-              <input onChange={(e)=>setEmail(e.target.value)} value={email} className="outline-none px-4 py-2 text-gray-500" placeholder="Your email" type="email"/>
-              {/* <input onChange={(e)=>setPassword(e.target.value)} value={password} className="outline-none px-4 py-2 text-gray-500" placeholder="Your password" type="password"/> */}
-              <div className="flex items-center space-x-4 mt-8">
-                <button onClick={handleUserUpdate} className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400">Update</button>
-                <button onClick={handleUserDelete} className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400">Delete</button>
-              </div>
-              {updated && <h3 className="text-green-500 text-sm text-center mt-4">user updated successfully!</h3>}
+    <div className="blkk">
+      <SideNavbar />
+      <div className="bluee">
+        <Navbar/>
+        <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
+          <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
+            <h1 className="text-xl font-bold mb-4">Your posts:</h1>
+            {posts?.map((p)=>(
+              <ProfilePosts key={p._id} p={p}/>
+            ))}
+          </div>
+          <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end ">
+            <div className=" flex flex-col space-y-4 items-start">
+                <h1 className="text-xl font-bold mb-4">Profile</h1>
+                <input onChange={(e)=>setUsername(e.target.value)} value={username} className="outline-none px-4 py-2 text-gray-500" placeholder="Your username" type="text"/>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} className="outline-none px-4 py-2 text-gray-500" placeholder="Your email" type="email"/>
+                {/* <input onChange={(e)=>setPassword(e.target.value)} value={password} className="outline-none px-4 py-2 text-gray-500" placeholder="Your password" type="password"/> */}
+                <div className="flex items-center space-x-4 mt-8">
+                  <button onClick={handleUserUpdate} className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400">Update</button>
+                  <button onClick={handleUserDelete} className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400">Delete</button>
+                </div>
+                {updated && <h3 className="text-green-500 text-sm text-center mt-4">user updated successfully!</h3>}
+            </div>
           </div>
         </div>
+        <Footer/>
       </div>
-      <Footer/>
     </div>
   )
 }
