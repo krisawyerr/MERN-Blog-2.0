188501de99afa613ed5690cbff1069de05777514
Kris Sawyerr <krisawyerr@gmail.com>
2023-10-22 10:41:15 -0700

    Made Navbar appear when sidenav disappears

--- Frontend/src/components/Menu.jsx
+++ Frontend/src/components/Menu.jsx
@@ -28,7 +28,7 @@ function Menu() {
         {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/register">Register</Link></h3>}
         {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/profile/"+user._id}>Profile</Link></h3>}
         {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/write">Write</Link></h3>}
-        {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/myblogs/"+user._id}>My blogs</Link></h3>}
+        {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/myblogs/"+user._id}>My Blogs</Link></h3>}
         {user && <h3 onClick={handleLogout} className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>}
 
         </div>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -19,25 +19,27 @@ function Navbar() {
   const {user}=useContext(UserContext)
 
   return (
-    <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-      <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blog Market</Link></h1>
-      { path === "/" && <div className="flex justify-center items-center space-x-0">
-        <p onClick={()=>navigate(prompt?"?search="+prompt:navigate("/"))} className="cursor-pointer"><BsSearch/></p>
-        <input onChange={(e)=>setPrompt(e.target.value)} className="outline-none px-3 " placeholder="Search a post" type="text"/>
-      </div>}
+    <div className="navbar">
+      <div className="navbarTitleAndMenu">
+        <div className="navbarTitle">
+          <h1><Link to="/">Blog Market</Link></h1>
+        </div>
 
-      <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4">
-        {user? <h3><Link to="/write">Write</Link></h3> :<h3><Link to="/login">Login</Link></h3>}
-        {user? 
-          <div onClick={showMenu} className="text-lg">
-            <p className="cursor-pointer relative"><FaBars/></p>
+        <div className="navbarMenu">
+          <div onClick={showMenu}>
+            <p><FaBars/></p>
             {menu && <Menu/>}
-          </div> :<h3><Link to="/register">Register</Link></h3>}
+          </div>
+        </div>
       </div>
-      <div onClick={showMenu} className="md:hidden text-lg">
-        <p className="cursor-pointer relative"><FaBars/></p>
-        {menu && <Menu/>}
+      <div className="navbarSearch">
+        { path === "/" && <div>
+          <p onClick={()=>navigate(prompt?"?search="+prompt:navigate("/"))}><BsSearch/></p>
+          <input onChange={(e)=>setPrompt(e.target.value)} placeholder="Search a post" type="text"/>
+        </div>}
       </div>
+
+      
     </div>
   )
 }
