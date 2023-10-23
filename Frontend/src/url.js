7d9d45766b5ded29b21b20a7309b9778d6122013
Kris Sawyerr <krisawyerr@gmail.com>
2023-10-23 14:47:53 -0700

    Deploy testing

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL='http://localhost:5000'
-export const IF='http://localhost:5000/images/'
+export const URL='https://technotes-api.onrender.com'
+export const IF='https://technotes-api.onrender.com/images/'
