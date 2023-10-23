8e2876de21d948ac444d2da2b57222db5b667d36
krisawyerr <krisawyerrr@gmail.com>
2023-10-23 13:30:33 -0700

    tailwind to css for login/reg

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -30,21 +30,21 @@ function Register() {
 
     return (
         <>
-            <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-                <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">T J C</Link></h1>
+            <div className="loginHeader">
+                <h1 className="loginTitle"><Link to="/">T J C</Link></h1>
                 <h3><Link to="/Login">Login</Link></h3>
             </div>
-            <div className="w-full flex justify-center items-center h-[80vh] ">
-                <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
-                    <h1 className="text-xl font-bold text-left">Create an account</h1>
-                    <input onChange={(e)=>setUsername(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your username" />
-                    <input onChange={(e)=>setEmail(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email" />
-                    <input onChange={(e)=>setPassword(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your password" />
-                    <button onClick={handleRegister} className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black ">Register</button>
-                    {error && <h3 className="text-red-500 text-sm ">Something went wrong</h3>}
-                    <div className="flex justify-center items-center space-x-3">
+            <div className="login">
+                <div className="loginForm">
+                    <h1 className="loginSpacing loginText">Create an account</h1>
+                    <input onChange={(e)=>setUsername(e.target.value)} className="loginSpacing loginInfo" type="text" placeholder="Enter your username" />
+                    <input onChange={(e)=>setEmail(e.target.value)} className="loginSpacing loginInfo" type="text" placeholder="Enter your email" />
+                    <input onChange={(e)=>setPassword(e.target.value)} className="loginSpacing loginInfo" type="password" placeholder="Enter your password" />
+                    <button onClick={handleRegister} className="loginSpacing loginButton">Register</button>
+                    {error && <h3 className="loginError">Something went wrong</h3>}
+                    <div className="loginNewHere">
                         <p>Alreadt have an account?</p>
-                        <p className="text-gray-500 hover:text-black"><Link to="/Login">Login</Link></p>
+                        <p className="loginNewHereLink"><Link to="/Login">Login</Link></p>
                     </div>
                 </div>
             </div>
