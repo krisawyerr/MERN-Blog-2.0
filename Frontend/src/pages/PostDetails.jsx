ee3854722c68447b38c83f4fcd31bb8222553b89
Kris Sawyerr <krisawyerr@gmail.com>
2023-10-22 17:32:38 -0700

    Finishing Touches

--- Frontend/src/pages/PostDetails.jsx
+++ Frontend/src/pages/PostDetails.jsx
@@ -9,6 +9,7 @@ import { URL,IF } from "../url"
 import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../context/UserContext"
 import Loader from "../components/Loader"
+import SideNavbar from "../components/sidenav"
 
 function PostDetails() {
 
@@ -90,48 +91,51 @@ function PostDetails() {
     }
 
     return (
-        <div>
-            <Navbar/>
-            {loader ? <div className="h-[80vh] flex justify-center items-center w-full"><Loader/></div> : <div className="px-8 md:px-[200px] mt-8">
-                <div className="flex justify-between items-center">
-                    <h1 className="text-2xl font-bold text-black md:text-3xl">{post.title}</h1>
-                    {user?._id === post?.userId && <div className="flex items-center justify-center space-x-2">
-                        <p onClick={()=>navigate("/edit/"+postId)} className="cursor-pointer"><BiEdit/></p>
-                        <p onClick={handleDeletePost} className="cursor-pointer"><MdDelete/></p>
-                    </div>}
-                </div>
-                <div className="flex items-center justify-between mt-2 md:mt-4">
-                    <p>@{post.username}</p>
-                    <div className="flex space-x-2 text-sm">
-                        <p>{new Date(post.updatedAt).toString().slice(0,15)}</p>
-                        <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
+        <div className="blkk">
+            <SideNavbar />
+            <div className="bluee">
+                <Navbar/>
+                {loader ? <div className="h-[80vh] flex justify-center items-center w-full"><Loader/></div> : <div className="px-8 md:px-[200px] mt-8">
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-2xl font-bold text-black md:text-3xl">{post.title}</h1>
+                        {user?._id === post?.userId && <div className="flex items-center justify-center space-x-2">
+                            <p onClick={()=>navigate("/edit/"+postId)} className="cursor-pointer"><BiEdit/></p>
+                            <p onClick={handleDeletePost} className="cursor-pointer"><MdDelete/></p>
+                        </div>}
                     </div>
-                </div>
-                <img src={IF + post.photo} className="w-full  mx-auto mt-8" alt=""/>
-                <p className="mx-auto mt-8">{post.description}</p>
-                <div className="flex items-center mt-8 space-x-4 font-semibold">
-                    <p>Categories:</p>
-                    <div className="flex justify-center items-center space-x-2">
-                        {post.catagories?.map((c,i)=>(
-                            <div key={i} className="bg-gray-300 rounded-lg px-3 py-1">
-                                {c}
-                            </div>
+                    <div className="flex items-center justify-between mt-2 md:mt-4">
+                        <p>@{post.username}</p>
+                        <div className="flex space-x-2 text-sm">
+                            <p>{new Date(post.updatedAt).toString().slice(0,15)}</p>
+                            <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
+                        </div>
+                    </div>
+                    <img src={IF + post.photo} className="w-full  mx-auto mt-8" alt=""/>
+                    <p className="mx-auto mt-8">{post.description}</p>
+                    <div className="flex items-center mt-8 space-x-4 font-semibold">
+                        <p>Categories:</p>
+                        <div className="flex justify-center items-center space-x-2">
+                            {post.catagories?.map((c,i)=>(
+                                <div key={i} className="bg-gray-300 rounded-lg px-3 py-1">
+                                    {c}
+                                </div>
+                            ))}
+                        </div>
+                    </div> 
+                    <div className="flex flex-col mt-4">
+                        <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
+                        {comments?.map((c)=>(
+                            <Comment key={c._id} c={c} post={post} />
                         ))}
-                    </div>
-                </div> 
-                <div className="flex flex-col mt-4">
-                    <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
-                    {comments?.map((c)=>(
-                        <Comment key={c._id} c={c} post={post} />
-                    ))}
-                </div>  
-                
-                <div className="w-full flex flex-col mt-4 md:flex-row">
-                    <input onChange={(e)=>setComment(e.target.value)} type="text" placeholder="Write a comment" className="md:w-[80%] outline-none py-2 px-4 mt-4 md:mt-0"/>
-                    <button onClick={postComment} className="bg-black text-sm text-white px-2 py-2 md:w-[20%] mt-4 md:mt-0">Add Comment</button>
-                </div>     
-            </div>}
-            <Footer/>
+                    </div>  
+                    
+                    <div className="w-full flex flex-col mt-4 md:flex-row">
+                        <input onChange={(e)=>setComment(e.target.value)} type="text" placeholder="Write a comment" className="md:w-[80%] outline-none py-2 px-4 mt-4 md:mt-0"/>
+                        <button onClick={postComment} className="bg-black text-sm text-white px-2 py-2 md:w-[20%] mt-4 md:mt-0">Add Comment</button>
+                    </div>     
+                </div>}
+                <Footer/>
+            </div>
         </div>
 
     )
