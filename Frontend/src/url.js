7a47f536e9cfbea6fbe36180c3302e546884571f
krisawyerr <144180836+krisawyerr@users.noreply.github.com>
2023-10-26 15:43:18 -0700

    Update url.js

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL='http://localhost:5000'
-export const IF='http://localhost:5000/images/'
+export const URL='https://travelersjournalcollection.netlify.app'
+export const IF='https://travelersjournalcollection.netlify.app/images/'
